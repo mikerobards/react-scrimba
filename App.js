4d0ac9a114e1e1ec53c8f0e6676e7692ef54e2aa
Michael Robards <mikerobards@icloud.com>
2021-07-16 16:41:00 -0400

    fooling around

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+// import React from 'react'
 import NavBar from './components/NavBar.jsx'
 import MainContent from './components/MainContent.jsx'
 import Footer from './components/Footer.jsx'
